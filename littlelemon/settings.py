20f255920d33069e9099885c43b1e8e505856e55
Shazil-khan <shazilkhannew@gmail.com>
2023-05-30 23:09:34 +0500

    new databases added

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'littlelemon',  
+        'USER': 'new_user',  
+        'PASSWORD': 'user123456',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
-
+# [client]
+# 'NAME': 'my_database'
+# 'USER': 'root'
+# 'PASSWORD': 'your_password'
+# 'default-character-set' = utf8
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -122,3 +133,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
